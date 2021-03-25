81fb3dbec9faef8240e5a8cfceba29013265ea82
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-03-25 00:10:03 -0400

    created map

---
+++ EscapeRoom/Assets/Scripts/PictureScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureScript : MonoBehaviour
+{
+
+    public Animation animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerStay()
+    {
+        if (Input.GetKey("e"))
+        {
+            animator.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
