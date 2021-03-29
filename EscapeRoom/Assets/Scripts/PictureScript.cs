c5a24a662e74344f0dd19a3310a5c733537e3276
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-03-29 19:27:52 -0400

    added animations

--- EscapeRoom/Assets/Scripts/PictureScript.cs
+++ EscapeRoom/Assets/Scripts/PictureScript.cs
@@ -5,24 +5,47 @@ using UnityEngine;
 public class PictureScript : MonoBehaviour
 {
 
-    public Animation animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform PlayerCamera;
+    [Header("MaxDistance you can open or close the door.")]
+    public float MaxDistance = 5;
 
-    void OnTriggerStay()
+    private bool opened = false;
+    private Animator anim;
+
+
+
+    void Update()
     {
-        if (Input.GetKey("e"))
+        //This will tell if the player press F on the Keyboard. P.S. You can change the key if you want.
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            animator.Play();
+            Pressed();
+            //Delete if you dont want Text in the Console saying that You Press F.
+            Debug.Log("You Press F");
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Pressed()
     {
-        
+        //This will name the Raycasthit and came information of which object the raycast hit.
+        RaycastHit doorhit;
+
+        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out doorhit, MaxDistance))
+        {
+
+            // if raycast hits, then it checks if it hit an object with the tag Door.
+            if (doorhit.transform.tag == "Picture")
+            {
+
+                //This line will get the Animator from  Parent of the door that was hit by the raycast.
+                anim = doorhit.transform.GetComponentInParent<Animator>();
+
+                //This will set the bool the opposite of what it is.
+                opened = !opened;
+
+                //This line will set the bool true so it will play the animation.
+                anim.SetBool("Moved", !opened);
+            }
+        }
     }
 }
